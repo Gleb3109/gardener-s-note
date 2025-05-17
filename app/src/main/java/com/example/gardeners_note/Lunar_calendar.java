3f40489a8f6a9c912b2fac954a03ffe8b2d89037
Gleb3109 <gleb.l3109@gmail.com>
2025-05-17 12:07:23 +0300

    Lunar_calendar

--- app/src/main/java/com/example/gardeners_note/Lunar_calendar.java
+++ app/src/main/java/com/example/gardeners_note/Lunar_calendar.java
@@ -2,11 +2,13 @@ package com.example.gardeners_note;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.math.*;
@@ -58,17 +60,23 @@ public class Lunar_calendar extends AppCompatActivity {
         TextView mtV2= findViewById(R.id.textView2);
         TextView mtV3= findViewById(R.id.textView5);
         TextView mtV4= findViewById(R.id.textView6);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView4);
+
         if (a==0){
             mtV.setText("Сейчас новолуние");
+            imageView.setImageResource(R.drawable.photo_5276016729706851909_m);
         }else if (a>0 && a<15){
             mtV.setText("Сейчас растущая луна, еще " +  (15-a) + " дней");
             mtV3.setText("Новолуние через "+(30-a) + " дней");
             mtV4.setText("Полнолуние через "+(15-a) + " дней");
+            imageView.setImageResource(R.drawable.photo_5276016729706851914_m);
         }else if (a==15){
             mtV.setText("Сейчас полнолуние");
+            imageView.setImageResource(R.drawable.photo_5276016729706851912_m);
         }else {
             mtV.setText("Сейчас убывающая луна, еще " +  (30-a) + " дней");
             mtV4.setText("Полнолуние через "+(45-a) + " дней");
+            imageView.setImageResource(R.drawable.photo_5276016729706851916_m);
         }
         mtV2.setText("Сегодня "+day+"."+month+"."+year);
         mtV3.setText("Новолуние через "+(30-a) + " дней");
